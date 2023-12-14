4d08f37ae6a86b3871758d0e19d755131ce6bb11
Alvaro Rodriguez Ruiz <itzalvi03@gmail.com>
2023-12-14 20:42:39 +0100

    Colores en los tipos y algunos recoques visuales

--- src/app/componentes/gameboy/pokedex/pokedex.component.ts
+++ src/app/componentes/gameboy/pokedex/pokedex.component.ts
@@ -81,6 +81,38 @@ export class PokedexComponent implements OnInit{
         return "rgb(61,162,36)";
       case 'POISON':
         return "rgb(146,63,204)";
+      case 'FIRE':
+        return "#E62425";
+      case 'FLYING':
+        return "#81B9EF";
+      case 'WATER':
+        return "#2980EF";
+      case 'BUG':
+        return "#91A119";
+      case 'NORMAL':
+        return "#A0A2A0";
+      case 'ELECTRIC':
+        return "#FAC000";
+      case 'GROUND':
+        return "#915121";
+      case 'FIGHTING':
+        return "#FF8000";
+      case 'PSYCHIC':
+        return "#EF4179";
+      case 'ROCK':
+        return "#AFA981";
+      case 'STEEL':
+        return "#61A1B8";
+      case 'ICE':
+        return "#3FD8FF";
+      case 'FAIRY':
+        return "#D38BB1";
+      case 'GHOST':
+        return "#642265";
+      case 'DRAGON':
+        return "#5061E1";
+      case 'DARK':
+        return "#50413F";
       default:
         return "rgb(0,0,0)";
     }
