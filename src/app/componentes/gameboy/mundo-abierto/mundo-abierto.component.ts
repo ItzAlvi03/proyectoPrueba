3cd49f3f363061c52f524902c183552621aeeb29
Alvaro Rodriguez Ruiz <itzalvi03@gmail.com>
2024-01-04 00:42:29 +0100

    Logica de colisiones, mapa totalmente responsive y se puede mover con el teclado

--- src/app/componentes/gameboy/mundo-abierto/mundo-abierto.component.ts
+++ src/app/componentes/gameboy/mundo-abierto/mundo-abierto.component.ts
@@ -1,38 +1,18 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 import { ArrayJsonService } from 'src/app/Services/Gameboy/MundoAbierto/array-json.service';
 import { ComunicationServiceService } from 'src/app/Services/Gameboy/comunication-service.service';
 
-class Sprite {
-  position = { x: 0, y: 0 };
-  context!: CanvasRenderingContext2D;
-  image;
-  constructor( position: any, velocity: any, image: any, context: CanvasRenderingContext2D) {
-    this.position = position;
-    this.image = image;
-    this.context = context;
-  }
-
-  draw(context: CanvasRenderingContext2D) {
-    context.drawImage(this.image, this.position.x, this.position.y);
-  }
-}
-
-class Boundary {
-  position = { x: 0, y: 0 };
-  static width = 48;
-  static height = 48;
-  context!: CanvasRenderingContext2D;
-
-  constructor(position: any, context: CanvasRenderingContext2D) {
-    this.position = position;
-    Boundary.width = 48;
-    Boundary.height = 48;
-    this.context = context;
-  }
-  draw(context: CanvasRenderingContext2D) {
-    context.fillStyle = 'red';
-    context.fillRect(this.position.x, this.position.y, Boundary.width, Boundary.height);
+class Collisions {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  constructor(x:number,y:number, width:number, height:number) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
   }
 }
 
@@ -41,150 +21,154 @@ class Boundary {
   templateUrl: './mundo-abierto.component.html',
   styleUrls: ['./mundo-abierto.component.css']
 })
-export class MundoAbiertoComponent implements AfterViewInit{
-  private destroyed$ = new Subject<void>();
-  private canvas: any;
-  context: any;
-  private image: any;
-  private player: any;
-  private collisions: any;
-  private lastKey = "";
-  private keys = {
-    ArrowUp: {
-      pressed: false
-    },
-    ArrowLeft: {
-      pressed: false
-    },
-    ArrowDown: {
-      pressed: false
-    },
-    ArrowRight: {
-      pressed: false
-    }
-  };
-  private background!: Sprite;
-  private boundaries!: any;
 
-  constructor(private comunicationService: ComunicationServiceService, private mundoAbierto: ArrayJsonService){}
+export class MundoAbiertoComponent implements AfterViewInit {
+  canvas: any;
+  ctx!: CanvasRenderingContext2D;
+  playerX: number = 0;
+  playerY: number = 0;
+  backgroundX: number = -90;
+  backgroundY: number = -107;
+  collisions!: Collisions[];
+  collisionW: number = 18;
+  collisionH: number = 18;
+  filas: number = 40;
+  columnas: number = 70;
+  mapa: any;
+  player: any;
+  array: any;
 
-  ngOnDestroy(): void {
-    document.removeEventListener('keydown', this.keydownListener);
-    document.removeEventListener('keyup', this.keyupListener);
-    this.destroyed$.next();
-    this.destroyed$.complete();
-  }
+  constructor(private service: ArrayJsonService) {}
 
   ngAfterViewInit(): void {
     this.canvas = document.querySelector('canvas');
-    this.context = this.canvas.getContext('2d');
-
-    this.collisions = this.mundoAbierto.getCollisions();
-
-    const collisionsMap = [];
-    for (let i = 0; i < this.collisions.length; i+=70){
-      collisionsMap.push(this.collisions.slice(i, 70 + i));
-    }
-    const offset = {
-      x: -7 * (this.canvas.width / 100),
-      y: -60 * (this.canvas.height / 100)
-    };
-    const position = {
-      x: offset.x,
-      y: offset.y
-    };
-    
-    this.image = new Image();
-    this.image.src = '../../../../assets/images/pruebaMapa.png';
+    this.ctx = this.canvas.getContext('2d');
+    this.playerX = this.canvas.width / 2;
+    this.playerY = this.canvas.height / 2;
+    this.collisions = [];
+    this.array = this.service.getCollisions();
+    //this.collisions[0] = new Collisions(100,190,120,80);
+    //this.collisions[1] = new Collisions(300,200,80,120);
+    this.mapa = new Image();
+    this.mapa.src = '../../../../assets/images/PokemonStyleGameMap.png';
     this.player = new Image();
     this.player.src = '../../../../assets/images/character.png';
-
-    document.addEventListener('keydown', this.keydownListener);
-    document.addEventListener('keyup', this.keyupListener);
-    this.comunicationService.accion.pipe(
-      takeUntil(this.destroyed$)
-    ).subscribe((event: string) => {
-      this.accionTecla(event);
-    });
-    this.background = new Sprite (position,0,this.image, this.context);
-    
-    this.animate()
-  }
-
-  private keydownListener = (event: KeyboardEvent) => {
-    this.accionTecla(event.key);
-  };
-  private keyupListener = (event: KeyboardEvent) => {
-    this.keyUp(event.key);
-  }
-
-  accionTecla(event: string) {
+    this.getCollisions();
+    this.drawMap();
+    this.drawPlayer();
+    this.handleInput();
     setTimeout(() => {
-      if(this.comunicationService.encendido.value){
-        switch (event){
-        case 'ArrowUp':
-          this.keys.ArrowUp.pressed = true;
-          this.lastKey = 'ArrowUp';
-          break;
-        case 'ArrowLeft':
-          this.keys.ArrowLeft.pressed = true;
-          this.lastKey = 'ArrowLeft';
-          break;
-        case 'ArrowDown':
-          this.keys.ArrowDown.pressed = true;
-          this.lastKey = 'ArrowDown';
-          break;
-        case 'ArrowRight':
-          this.keys.ArrowRight.pressed = true;
-          this.lastKey = 'ArrowRight';
-          break;
-        }
-      } else{
-        this.ngOnDestroy();
-      }
-    },50);
+      this.nuevoFrame();
+    }, 50);
   }
 
-  keyUp(event: string){
-    setTimeout(() => {
-      if(this.comunicationService.encendido.value){
-        switch (event){
-        case 'ArrowUp':
-          this.keys.ArrowUp.pressed = false;
-          break;
-        case 'ArrowLeft':
-          this.keys.ArrowLeft.pressed = false;
-          break;
-        case 'ArrowDown':
-          this.keys.ArrowDown.pressed = false;
-          break;
-        case 'ArrowRight':
-          this.keys.ArrowRight.pressed = false;
-          break;
-        }
-      } else{
-        this.ngOnDestroy();
-      }
-    },50);
-  }
-
-  animate(): any{
-    window.requestAnimationFrame(() => this.animate());
-    this.background.draw(this.context);
-    this.context.drawImage(this.player,
-      0,
-      0,
-      this.player.width,
-      this.player.height,
-      this.canvas.width / 2 - this.player.width / 2,
-      this.canvas.height / 2 - this.player.height / 2,
-      this.player.width,
-      this.player.height
-    );
+  getCollisions() {
+    let i = 0;
+    let num = 0;
   
-    if (this.keys.ArrowUp.pressed && this.lastKey === 'ArrowUp') this.background.position.y += 3;
-    if (this.keys.ArrowDown.pressed && this.lastKey === 'ArrowDown') this.background.position.y -= 3;
-    if (this.keys.ArrowLeft.pressed && this.lastKey === 'ArrowLeft') this.background.position.x += 3;
-    if (this.keys.ArrowRight.pressed && this.lastKey === 'ArrowRight') this.background.position.x -= 3;
+    for (let fila = 0; fila < this.filas; fila++) {
+      for (let columna = 0; columna < this.columnas; columna++) {
+        if (this.array[i] === 1025) {
+          
+          const x = columna * this.collisionW - (this.collisionW  * 5);
+          const y = fila * this.collisionH - (this.collisionH * 6.2);
+  
+          this.collisions[num] = new Collisions(x, y, this.collisionW, this.collisionH);
+          num++;
+        }
+        i++;
+      }
+    }
+
+  }
+
+  drawMap(): void {
+    // Dibuja el mapa y las áreas de colisión en rojo
+    this.ctx.drawImage(this.mapa, this.backgroundX, this.backgroundY);
+
+    // Áreas de colisión en rojo
+    //this.ctx.fillStyle = 'red';
+    // Áreas de colisión transparentes
+    this.ctx.fillStyle = 'rgba(0, 0, 0, 0.0)';
+    for(let i = 0; i < this.collisions.length; i++){
+      this.ctx.fillRect(this.collisions[i].x, this.collisions[i].y, this.collisions[i].width, this.collisions[i].height);
+    }
+  }
+
+  drawPlayer(): void {
+    // Dibuja el jugador
+    this.ctx.drawImage(this.player, this.playerX, this.playerY);
+
+  }
+
+  handleInput(): void {
+    // Maneja la entrada del teclado para mover al jugador
+    window.addEventListener('keydown', (event) => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.movePlayer(-5, 0);
+          break;
+        case 'ArrowRight':
+          this.movePlayer(5, 0);
+          break;
+        case 'ArrowUp':
+          this.movePlayer(0, -5);
+          break;
+        case 'ArrowDown':
+          this.movePlayer(0, 5);
+          break;
+      }
+    });
+  }
+
+  movePlayer(dx: number, dy: number): void {
+    // Mueve al jugador
+    this.backgroundX -= dx;
+    this.backgroundY -= dy;
+    for (let i = 0; i < this.collisions.length; i++) {
+      this.collisions[i].x -= dx;
+      this.collisions[i].y -= dy;
+    }
+    
+    if (this.checkCollisions()) {
+      // Revierte la posición del jugador en lugar de la del fondo
+      this.backgroundX += dx;
+      this.backgroundY += dy;
+      for (let i = 0; i < this.collisions.length; i++) {
+        this.collisions[i].x += dx;
+        this.collisions[i].y += dy;
+      }
+    }
+
+    this.nuevoFrame();
+  }
+  
+
+  checkCollisions(): boolean {
+    // Verifica las colisiones con las áreas rojas si hay colision devuelve true
+    let colision = false;
+    for (let i = 0; i < this.collisions.length; i++) {
+      if (
+        this.playerX < this.collisions[i].x + this.collisions[i].width &&
+        this.playerX + this.player.width > this.collisions[i].x &&
+        this.playerY < this.collisions[i].y + this.collisions[i].height &&
+        this.playerY + this.player.height > this.collisions[i].y
+      ) {
+        // Colisión detectada
+        colision = true;
+      }
+    }
+
+    return colision;
+  }
+   nuevoFrame(): void{
+    this.clearCanvas();
+    this.drawMap();
+    this.drawPlayer();
+   }
+
+  clearCanvas(): void {
+    // Limpia el lienzo
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
