7c537cda987274d1da5eee4ba985147816b3db2e
FRESHTRATEGY\alvaro.rodriguez <alvaro.rodriguez@freshtrategy.com>
2024-01-11 16:28:26 +0100

    Cambios en la pantalla principal

--- src/app/componentes/principal/principal.component.ts
+++ src/app/componentes/principal/principal.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { APIServiceService } from 'src/app/Services/apiservice.service';
 
 @Component({
@@ -6,7 +7,7 @@ import { APIServiceService } from 'src/app/Services/apiservice.service';
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css']
 })
-export class PrincipalComponent{
+export class PrincipalComponent implements OnInit, AfterViewInit{
   usando: boolean = false;
   divMensaje: any;
   mensaje: any;
@@ -17,8 +18,24 @@ export class PrincipalComponent{
   area: any;
   version: any;
   localTime: any;
-  constructor(private service: APIServiceService){}
-
+  title: string = '<portfolio/>';
+  menu: boolean = false;
+  isMobile = true;
+  constructor(private service: APIServiceService,
+    private router: Router){}
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkWindowSize();
+  }
+  
+  ngOnInit() {
+    this.checkWindowSize();
+  }
+  
+  checkWindowSize() {
+    this.isMobile = window.innerWidth <= 900;
+  }
   ngAfterViewInit() {
     this.divMensaje = document.getElementById('mensaje');
   }
@@ -41,29 +58,33 @@ export class PrincipalComponent{
     setTimeout(() => {
         this.divMensaje.style.right = '-100%';
         this.divMensaje.style.opacity = '0';
-    }, 3000);
+      }, 3000);
+      //Se espera 3 seg hata que se esconda del todo y pueda volver a usarse
+      setTimeout(() => {
+        this.usando = false;
+      }, 3000);
+    }
+    
+    mensajeConfirmacion(mensaje: string) {
+      this.usando = true;
+      this.mensaje = mensaje;
+      this.divMensaje.style.opacity = '1';
+      this.divMensaje.style.right = '20px';
+      this.divMensaje.style.backgroundColor = '#76ff7b';
+      this.divMensaje.style.color = '#0e0000';
+      //Se espera 3 seg hasta hacer la acción de esconder otra vez el mensaje
+      setTimeout(() => {
+        this.divMensaje.style.right = '-100%';
+        this.divMensaje.style.opacity = '0';
+      }, 3000);
     //Se espera 3 seg hata que se esconda del todo y pueda volver a usarse
     setTimeout(() => {
       this.usando = false;
     }, 3000);
   }
-
-  mensajeConfirmacion(mensaje: string) {
-    this.usando = true;
-    this.mensaje = mensaje;
-    this.divMensaje.style.opacity = '1';
-    this.divMensaje.style.right = '20px';
-    this.divMensaje.style.backgroundColor = '#76ff7b';
-    this.divMensaje.style.color = '#0e0000';
-    //Se espera 3 seg hasta hacer la acción de esconder otra vez el mensaje
-    setTimeout(() => {
-      this.divMensaje.style.right = '-100%';
-      this.divMensaje.style.opacity = '0';
-    }, 3000);
-    //Se espera 3 seg hata que se esconda del todo y pueda volver a usarse
-    setTimeout(() => {
-      this.usando = false;
-    }, 3000);
+  entrarAnimacion(route: string, num: number) {
+    const img = document.querySelector('.' + num);
+    this.router.navigate([route])
   }
 }
 
